b3fdc4bce636f107021c631cf4afc8188f66f200
Beltran <beltran@pantoja.cl>
2026-06-22 15:38:05 -0400

    Added design matrix functions

---
+++ R/design_matrix.R
@@ -0,0 +1,81 @@
+#' Create design matrix
+#'
+#' It creates a design matrix based on the passed rule. This function is useful
+#'  for the 1PLCDM implementation and the TDCM function and is not really meant
+#'  to be used as a standalone.
+#'
+#' @param qmatrix a required \eqn{I \times A} matrix indicating which items
+#'  measure which attributes.
+#' @param rule A string or a vector of itemwise condensation rules that
+#'  specific DCM to estimate.
+#'
+#' @returns A design matrix to be used as `delta.designmatrix` parameter when
+#'  calling [CDM::gdina()].
+#'
+#' @keywords internal
+#' @noRd
+create_base_design_matrix <- function(qmatrix, rule) {
+  if (rule == "1PLCDM") {
+    design_matrix <- design_matrix_1plcdm(qmatrix)
+  } else {
+    # Fake response data to pass to `tdcm.base`
+    data <- matrix(
+      sample(c(0, 1), 1000 * ncol(qmatrix), replace = TRUE),
+      ncol = ncol(qmatrix)
+    )
+
+    # This creates a dummy model that we can use to handle the
+    # number of parameters that come from the `rule` argument.
+    m0 <- TDCM:::tdcm.base(data, qmatrix, rule)
+
+    # Base design matrix
+    design_matrix <- diag(nrow(m0$coef))
+  }
+
+  # Return
+  design_matrix
+}
+
+
+#' Create design matrix for the 1PLCDM model
+#'
+#' It creates a design matrix for the 1PLCDM model where the main effects are
+#'  restricted.
+#'
+#' @inheritParams create_base_design_matrix qmatrix
+#'
+#' @inheritSection create_base_design_matrix returns
+#'
+#' @keywords internal
+#' @noRd
+design_matrix_1plcdm <- function(qmatrix) {
+  # Check properly built
+  if (any(rowSums(qmatrix) > 1)) {
+    stop("All items should be simple structure.")
+  }
+
+  # Make a design matrix for each attribute
+  simple_design_matrices <- lapply(
+    colSums(qmatrix),
+    FUN = function(num_items) {
+      # The general design matrix
+      design_matrix <- diag(num_items * 2)
+
+      # We get all the indices of the main effects.
+      main_effects_idx <- seq(2, nrow(design_matrix), by = 2)
+
+      # We make the main effects be all equal to the first main effect.
+      design_matrix[main_effects_idx, ] <- 0
+      design_matrix[seq(2, nrow(design_matrix), by = 2), 2] <- 1
+
+      # Return without the empty columns.
+      remove_cols <- which(colSums(design_matrix) == 0)
+      design_matrix[, -remove_cols]
+    }
+  )
+
+  # Join them diagonally
+  as.matrix(
+    Matrix::bdiag(simple_design_matrices)
+  )
+}
---
+++ R/tdcm.design_matrix.R
@@ -0,0 +1,75 @@
+#' Create design matrix for TDCM
+#'
+#' This calls `create_base_design_matrix` and also handles the anchoring.
+#'  Invariance is a simple case in which we replicate a single design matrix so
+#'  it is handled on the main function.
+#'
+#' @param qmatrix_list a list of \eqn{I \times A} matrices indicating which
+#'  items measure which attributes. This function assumes that if there's a
+#'  single qmatrix it was already.
+#' @param anchors a list of items to be anchored. They should be given as pairs.
+#'  (e.g. `c(1,11,3,15)` would imply that items 1 and 11 are the same and 3 and
+#'  3 and 15 too).
+#'
+#' @inheritParams create_base_design_matrix rule
+#'
+#' @inheritSection create_base_design_matrix returns
+#'
+#' @keywords internal
+#' @noRd
+design_matrix_TDCM <- function(
+  qmatrix_list,
+  anchors = NULL,
+  rule = "GDINA"
+) {
+  # We create base design matrices for all Q-matrices
+  design_matrices <- lapply(
+    qmatrix_list,
+    function(qmat) {
+      create_base_design_matrix(qmat, rule)
+    }
+  )
+
+  # Then we join it into a single design matrix
+  design_matrix <- Matrix::.bdiag(design_matrices)
+
+
+  # If there are no anchors then we just return the design matrix
+  if (is.null(anchors)) {
+    return(design_matrix)
+  }
+
+  # ======================================================
+  # ANCHORING LOGIC
+  # ======================================================
+
+  # We create the table of the parameters to guide the anchoring process
+
+  qnew <- Matrix::.bdiag(qmatrix_list)
+  data <- matrix(
+    sample(c(0, 1), 1000 * ncol(qnew), replace = TRUE),
+    ncol = ncol(qnew)
+  )
+  c0 <- TDCM:::tdcm.base(data, qnew, rule)$coef
+
+
+  # The anchoring matrix established the anchor pairs
+  anchor_matrix <- matrix(anchor, ncol = 2, byrow = TRUE)
+
+  # We iterate over each each anchor-pair
+  for (row in seq_len(nrow(anchor_matrix))) {
+    # items have multiple parameters, so we get the indices for the source and target
+    source_item_idx <- which(c0[, "itemno"] == anchor_matrix[row, 1])
+    target_item_idx <- which(c0[, "itemno"] == anchor_matrix[row, 2])
+
+    design_matrix[target_item_idx, ] <- design_matrix[source_item_idx, ]
+  }
+
+
+  # The constrained items will correspond to columns with only 0s
+  remove_cols <- which(colSums(design_matrix) == 0)
+
+
+  # Return
+  design_matrix[, -remove_cols]
+}
